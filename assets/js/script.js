81c291649a2b25266f87c4ee8e8420ad3ed97c36
amitpaultl <amitpaultl@gmail.com>
2024-09-15 17:06:04 +0600

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,5 +30,23 @@
 	animate menu area end
 	========================*/
 
+	$(window).scroll(function() {
+		var windscroll = $(window).scrollTop();
+		if (windscroll >= 100) {
+			$('.scroll-section').each(function(i) {
+				if ($(this).position().top <= windscroll - 0) {
+					$('.bd-social-nav-maintop a.active').removeClass('active');
+					$('.bd-social-nav-maintop a').eq(i).addClass('active');
+				}
+			});
+
+		} else {
+
+			$('.bd-social-nav-maintop a.active').removeClass('active');
+			$('.bd-social-nav-maintop a:first').addClass('active');
+		}
+
+	}).scroll();
+
 
 })(window.jQuery);
