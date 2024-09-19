f33b7698267a7d698a2c9511daaeb6bb26f280a9
amitpaultl <amitpaultl@gmail.com>
2024-09-19 18:20:50 +0600

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,12 +29,15 @@
 	/*=======================
 	animate menu area end
 	========================*/
+	/*=======================
+	scroll menu area start
+	========================*/
 
 	$(window).scroll(function() {
 		var windscroll = $(window).scrollTop();
 		if (windscroll >= 100) {
 			$('.scroll-section').each(function(i) {
-				if ($(this).position().top <= windscroll - 0) {
+				if ($(this).position().top <= windscroll - 1) {
 					$('.bd-social-nav-maintop a.active').removeClass('active');
 					$('.bd-social-nav-maintop a').eq(i).addClass('active');
 				}
@@ -47,6 +50,44 @@
 		}
 
 	}).scroll();
+
+	/*=======================
+	scroll menu area end
+	========================*/
+
+	/*=======================
+	login menu area start
+	========================*/
+
+	$(document).ready(function () {
+
+		$("#bd-social-login").click(function(){
+			
+			$(".bd-social-sing-up").addClass("intro-login-rotate");
+			$(".bd-social-sing-up").removeClass("intro-sing-in-social");
+			$(".bd-social-sing-in").addClass("intro-login-social");
+			setTimeout(function() {
+				$(".bd-social-sing-in").addClass("z-index-part");
+			}, 1000);
+			
+		});
+
+		$("#bd-social-sing-up").click(function(){
+			$(".bd-social-sing-up").addClass("intro-sing-in-social");
+			$(".bd-social-sing-up").removeClass("intro-login-rotate");
+			$(".bd-social-sing-in").removeClass("intro-login-social");
+			setTimeout(function() {
+				$(".bd-social-sing-in").removeClass("z-index-part");
+			}, 1000);
+			setTimeout(function() {
+				$(".bd-social-sing-up").addClass("z-index-part");
+			}, 1000);
+		});
+	})
+
+	/*=======================
+	login menu area end
+	========================*/
 
 
 })(window.jQuery);
